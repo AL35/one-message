9a5485f6036cec8851a07e523588e19f0ccf1778
wessamsalah <wessamsalah@live.com>
2017-06-09 22:38:25 +0200

    fix edit user navigate after edit

--- app/src/main/java/onemessagecompany/onemessage/Admin/EditUserActivity.java
+++ app/src/main/java/onemessagecompany/onemessage/Admin/EditUserActivity.java
@@ -1,5 +1,6 @@
 package onemessagecompany.onemessage.Admin;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,6 +15,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import onemessagecompany.onemessage.AdminMainActivity;
 import onemessagecompany.onemessage.R;
 import onemessagecompany.onemessage.data.sharedData;
 import onemessagecompany.onemessage.model.EditUserRequest;
@@ -27,97 +29,100 @@ import retrofit2.Response;
 
 public class EditUserActivity extends AppCompatActivity {
 
-  private User user;
-  private EditText mEditUserName;
-  private EditText mFirstName;
-  private EditText mLastName;
-  private CheckBox mRegisterActivateCheckbox;
+    private User user;
+    private EditText mEditUserName;
+    private EditText mFirstName;
+    private EditText mLastName;
+    private CheckBox mRegisterActivateCheckbox;
+    private Context context = EditUserActivity.this;
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE);
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
-      WindowManager.LayoutParams.FLAG_SECURE);
+        setContentView(R.layout.activity_edit_user);
 
-    setContentView(R.layout.activity_edit_user);
-
-    User user = (User) getIntent().getSerializableExtra("userDetails");
-
-
-    mEditUserName = (EditText) findViewById(R.id.edit_UserName);
-    mFirstName = (EditText) findViewById(R.id.edit_FirstName);
-    mLastName = (EditText) findViewById(R.id.edit_LastName);
-    mRegisterActivateCheckbox = (CheckBox) findViewById(R.id.edit_checkBox);
-
-    mEditUserName.setText(user.getUserName());
-    mEditUserName.setFocusable(false);
-    mFirstName.setText(user.getFirstName());
-    mLastName.setText(user.getLastName());
-    mRegisterActivateCheckbox.setChecked(user.getIsEnabled());
-
-    Button mRegisterButton = (Button) findViewById(R.id.edit_button);
-    mRegisterButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        EditPost();
-      }
-    });
-
-  }
-
-  private void EditPost() {
-
-    String userName = mEditUserName.getText().toString();
-    String firstName = mFirstName.getText().toString();
-    String lastName = mLastName.getText().toString();
-    boolean registerEnabled = mRegisterActivateCheckbox.isChecked();
+        User user = (User) getIntent().getSerializableExtra("userDetails");
 
 
+        mEditUserName = (EditText) findViewById(R.id.edit_UserName);
+        mFirstName = (EditText) findViewById(R.id.edit_FirstName);
+        mLastName = (EditText) findViewById(R.id.edit_LastName);
+        mRegisterActivateCheckbox = (CheckBox) findViewById(R.id.edit_checkBox);
 
-    UsersApi  usersApi= ApiClient.getAuthorizedClient().create(UsersApi.class);
-    EditUserRequest editUserRequest = new EditUserRequest();
+        mEditUserName.setText(user.getUserName());
+        mEditUserName.setFocusable(false);
+        mFirstName.setText(user.getFirstName());
+        mLastName.setText(user.getLastName());
+        mRegisterActivateCheckbox.setChecked(user.getIsEnabled());
 
-    editUserRequest.setUserName(userName);
-    editUserRequest.setFirstName(firstName);
-    editUserRequest.setLastName(lastName);
-    editUserRequest.setEnabled(registerEnabled);
+        Button mRegisterButton = (Button) findViewById(R.id.edit_button);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditPost();
+            }
+        });
 
-
-    usersApi.EditUser(editUserRequest).enqueue(new Callback<EditUserResponse>() {
-      @Override
-      public void onResponse(Call<EditUserResponse> call, Response<EditUserResponse> response) {
-        int statusCode = response.code();
-        if (statusCode == 200 && response.body().isUserChanged())
-          Toast.makeText(getApplicationContext(), "Saved Success", Toast.LENGTH_LONG).show();
-      }
-
-      @Override
-      public void onFailure(Call<EditUserResponse> call, Throwable t) {
-        Toast.makeText(getApplicationContext(), "Invalid Login username or password", Toast.LENGTH_LONG).show();
-      }
-    });
-
-  }
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case android.R.id.home:
-        onBackPressed();
-        return true;
-      default:
-        return true;
     }
-  }
+
+    private void EditPost() {
+
+        String userName = mEditUserName.getText().toString();
+        String firstName = mFirstName.getText().toString();
+        String lastName = mLastName.getText().toString();
+        boolean registerEnabled = mRegisterActivateCheckbox.isChecked();
 
 
-  @Override
-  public void onBackPressed() {
-    Intent intent = new Intent();
-    intent.putExtra("userDetails", user);
-    setResult(RESULT_OK, intent);
-    finish();
-  }
+        UsersApi usersApi = ApiClient.getAuthorizedClient().create(UsersApi.class);
+        EditUserRequest editUserRequest = new EditUserRequest();
+
+        editUserRequest.setUserName(userName);
+        editUserRequest.setFirstName(firstName);
+        editUserRequest.setLastName(lastName);
+        editUserRequest.setEnabled(registerEnabled);
+
+
+        usersApi.EditUser(editUserRequest).enqueue(new Callback<EditUserResponse>() {
+            @Override
+            public void onResponse(Call<EditUserResponse> call, Response<EditUserResponse> response) {
+                int statusCode = response.code();
+                if (statusCode == 200 && response.body().isUserChanged()) {
+                    Toast.makeText(getApplicationContext(), "Saved Success", Toast.LENGTH_LONG).show();
+                    Intent userMain = new Intent(context, AdminMainActivity.class);
+                    startActivity(userMain);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<EditUserResponse> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "Invalid Login username or password", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return true;
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("userDetails", user);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
 
 }
