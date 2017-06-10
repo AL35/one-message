7eafcf0473fe94ad0316024130c9481fb426f712
Ahmed Lotfy <lotfy.ahmed@hotmail.com>
2017-06-10 05:53:27 +0200

    some nice UI(Dark)

--- app/src/main/java/onemessagecompany/onemessage/Admin/ChangePasswordActivity.java
+++ app/src/main/java/onemessagecompany/onemessage/Admin/ChangePasswordActivity.java
@@ -2,19 +2,17 @@ package onemessagecompany.onemessage.Admin;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import onemessagecompany.onemessage.R;
-import onemessagecompany.onemessage.data.sharedData;
 import onemessagecompany.onemessage.model.ChangeUserPasswordRequest;
 import onemessagecompany.onemessage.model.User;
 import onemessagecompany.onemessage.rest.ApiClient;
@@ -39,6 +37,16 @@ public class ChangePasswordActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_SECURE);
 
         setContentView(R.layout.activity_change_password);
+
+        findViewById(R.id.ac_change_password).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                return true;
+            }
+        });
+
         user = (User) getIntent().getSerializableExtra("userDetails");
 
         mPassword = (EditText) findViewById(R.id.edit_password);
--- app/src/main/java/onemessagecompany/onemessage/Public/SendMessageActivity.java
+++ app/src/main/java/onemessagecompany/onemessage/Public/SendMessageActivity.java
@@ -1,25 +1,16 @@
 package onemessagecompany.onemessage.Public;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import onemessagecompany.onemessage.R;
-import onemessagecompany.onemessage.data.sharedData;
 import onemessagecompany.onemessage.model.SendMessageResponse;
 import onemessagecompany.onemessage.rest.ApiClient;
 import onemessagecompany.onemessage.rest.SendMessageApi;
@@ -38,7 +29,14 @@ public class SendMessageActivity extends AppCompatActivity {
 
     setContentView(R.layout.activity_send_message);
 
-
+    findViewById(R.id.login_form).setOnTouchListener(new View.OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        return true;
+      }
+    });
     Button btnSnd = (Button) findViewById(R.id.btnSendMessage);
 
 
