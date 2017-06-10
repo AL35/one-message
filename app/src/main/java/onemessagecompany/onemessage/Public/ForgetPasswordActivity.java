04f233782609663fd8b39a72b14658b509e6e1f2
Ahmed Lotfy <lotfy.ahmed@hotmail.com>
2017-06-11 00:57:34 +0200

    forget password hide keyboard

--- app/src/main/java/onemessagecompany/onemessage/Public/ForgetPasswordActivity.java
+++ app/src/main/java/onemessagecompany/onemessage/Public/ForgetPasswordActivity.java
@@ -1,16 +1,14 @@
 package onemessagecompany.onemessage.Public;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import onemessagecompany.onemessage.Adapters.ForgetPasswordAdapter;
 import onemessagecompany.onemessage.R;
 import onemessagecompany.onemessage.model.ForgetPasswordRequest;
 import onemessagecompany.onemessage.model.ForgetPasswordResponse;
@@ -26,6 +24,16 @@ public class ForgetPasswordActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_forget_password);
+
+    findViewById(R.id.login_form).setOnTouchListener(new View.OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        return true;
+      }
+    });
+
 
     Button btnForgetPassword = (Button) findViewById(R.id.btnForgetPassword);
     btnForgetPassword.setOnClickListener(new View.OnClickListener() {
