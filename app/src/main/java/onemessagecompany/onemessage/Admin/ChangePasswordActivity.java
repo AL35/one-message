15705641d4c1a33e5dbe7dd46ffc15e308181d42
Ahmed Lotfy <lotfy.ahmed@hotmail.com>
2017-06-10 05:54:45 +0200

    very minor change

--- app/src/main/java/onemessagecompany/onemessage/Admin/ChangePasswordActivity.java
+++ app/src/main/java/onemessagecompany/onemessage/Admin/ChangePasswordActivity.java
@@ -2,10 +2,7 @@ package onemessagecompany.onemessage.Admin;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
@@ -14,7 +11,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import onemessagecompany.onemessage.R;
-import onemessagecompany.onemessage.data.sharedData;
 import onemessagecompany.onemessage.model.ChangeUserPasswordRequest;
 import onemessagecompany.onemessage.model.User;
 import onemessagecompany.onemessage.rest.ApiClient;
