41b2b32719546e6651d0009bcacd3dcd3bc39949
Ahmed Lotfy <lotfy.ahmed@hotmail.com>
2017-06-11 11:25:22 +0200

    fix message details

--- app/src/main/java/onemessagecompany/onemessage/Public/MessageDetailsActivity.java
+++ app/src/main/java/onemessagecompany/onemessage/Public/MessageDetailsActivity.java
@@ -2,14 +2,14 @@ package onemessagecompany.onemessage.Public;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +37,16 @@ public class MessageDetailsActivity extends AppCompatActivity {
       WindowManager.LayoutParams.FLAG_SECURE);
 
     setContentView(R.layout.activity_message_details);
+
+    findViewById(R.id.message_details_form).setOnTouchListener(new View.OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        return true;
+      }
+    });
+
     message = (Message) getIntent().getSerializableExtra("message");
 
     TextView txtMsg = (TextView) findViewById(R.id.txt_current_Message);
@@ -46,7 +56,7 @@ public class MessageDetailsActivity extends AppCompatActivity {
 
     seenMessage();
 
-    Button btnSendReply = (Button) findViewById(R.id.btnSendReply);
+    ImageButton btnSendReply = (ImageButton) findViewById(R.id.btnSendReply);
     Button btnDelete = (Button) findViewById(R.id.btnDeleteMsg);
     Button btnDismiss = (Button) findViewById(R.id.btnDismiss);
 
